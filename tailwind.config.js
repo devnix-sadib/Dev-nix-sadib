164be716d3009dce3591264aab260b3c8bad513c
Dev-sadib <mohammedsadib8@gmail.com>
2025-01-12 12:44:15 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+
+
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        customGreen: '#ecf0f3',
+        redcolor:'#FF014F' // Add your custom color here
+      },
+    },
+  },
+  plugins: [],
+};
